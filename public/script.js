82dd54aed6b5f746ce34a680321c7d35c8e3d171
Kuilic <109625322+kaneki-code@users.noreply.github.com>
2025-04-08 18:33:38 +0000

    terrible

--- public/script.js
+++ public/script.js
@@ -1,5 +1,14 @@
 // Create a function that tests the post route handler [2 pts]
+console.log("Script running...")
+const base = "https://glorious-adventure-qjp4q5r99p6h9746-3000.app.github.dev"
+// Task 1: Complete the code to make a fetch call to the /creatures endpoint
+async function potion(){
+    const response = await fetch(base +"/creatures")
+    const data = await response.json()
+    console.log(data)
+}
 
+potion()
 
 
 // Call the function [1 pt]
